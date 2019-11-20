13752512a245a2a3cc5ba03b6dc05ec4685a5bfd
Richard Willian Ribeiro Divino <richard.divino@useargo.com>
2019-11-20 18:10:30 -0300

    Adiciona 'listview' para apresentar as mensagens

--- eco_dicas/lib/ui/home_page.dart
+++ eco_dicas/lib/ui/home_page.dart
@@ -29,15 +29,37 @@ class _HomePageState extends State<HomePage> {
       backgroundColor: Colors.white,
       body: ListView.builder(
         padding: EdgeInsets.all(15.0),
-        itemCount: 15,
+        itemCount: 10,
         itemBuilder: (context, index) {
           return GestureDetector(
             child: Card(
               child: Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Row(children: <Widget>[
-
-                ],),
+                padding: EdgeInsets.only(top: 15.0),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      width: 5.0,
+                      height: 5.0,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: null,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "Utilize a água somente o necessário. Só 2,5% da água do Planeta Terra é doce e destes, \nsomente 0,01% é utilizável pelo homem. \nPortanto, nunca lave uma calçada utilizando uma mangueira. \nUse um balde;",
+                            style: TextStyle(fontSize: 8.0, fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           );
