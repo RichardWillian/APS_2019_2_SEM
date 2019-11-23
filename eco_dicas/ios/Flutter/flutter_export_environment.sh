1c3189a29a279b3be8fa8710b5dedf0ed6330ee5
Richard Willian <richardwillian.2008@hotmail.com>
2019-11-22 23:16:38 -0300

    Adiciona imagens da monosgrafia em pasta e melhora escrita da monosgrafia adicionando as referências

--- eco_dicas/ios/Flutter/flutter_export_environment.sh
+++ eco_dicas/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\richard.divino\Desktop\Projetos\Flutter\APS_4_ANO_2_SEM\eco_dicas"
+export "FLUTTER_ROOT=D:\Downloads\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Projetos\Flutter\APS_4_ANO_2_SEM\eco_dicas"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\Downloads\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
